1cfd8e0a85fe2023a37d565327b252af2e1127c5
Sandip <85915938+Sandipop@users.noreply.github.com>
2025-09-17 11:36:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Add the base parameter here 👇
+      base: "/TNT-POSTS-TO-DESIGN",
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
